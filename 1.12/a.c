3599843d0fc902c5dbf0587217fdd739c4353e44
Hannes Tydén <hannes@soundcloud.com>
2011-07-03 16:49:28 +0200

    Solutions for section 1.5.4.

---
+++ 1.12/a.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define IN	1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+int main() {
+  int c, state;
+
+  state = OUT;
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\n' || c == '\t') {
+      if (state == IN)
+        putchar('\n');
+      state = OUT;
+    } else {
+      state = IN;
+      putchar(c);
+    }
+  }
+
+  return 0;
+}
